d010a3ae1d854ba736100c14cfe7b37ebadf003a
Ubuntu <ubuntu@ip-172-31-80-80.ec2.internal>
2021-10-28 01:44:14 +0000

    fixed 680

--- Unsorted/680 Valid Palindrome II.py
+++ Unsorted/680 Valid Palindrome II.py
@@ -10,8 +10,34 @@ class Solution:
                 check if past mistake
                 if past mistake -> return false
                 if no past mistake, 
-                    try to find a valid variation
-                    if found -> continue
-                    if none -> return false
+                    return whether s[left:right] or s[left+1:right+1] is a perfect palindrome
         return true
         """
+        mistakes = 0
+        left = 0
+        right = len(s)-1
+
+        while left < right:
+            if s[left] == s[right]:
+                left+=1
+                right-=1
+                continue
+
+            if mistakes > 0:
+                print("mistake")
+                print(s[left], left, s[right], right)
+                return False
+
+            return self.perfectPalindrome(s[left:right]) or self.perfectPalindrome(s[left+1:right+1])
+        
+        return True
+
+    def perfectPalindrome(selft, s:str):
+        left = 0
+        right = len(s)-1
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left+=1
+            right-=1
+        return True
